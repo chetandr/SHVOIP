b393d83aae94806389222b2c1c5c0a4061d88985
Chetan Dattaram Rane <chetan.d.rane@gmail.com>
2014-07-18 02:08:04 +0530

    Intial Commit

---
+++ inc/h.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="iso-8859-1, windows-1252">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shvoip Voice</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--  <link href="css/bootstrap-theme.min.css" rel="stylesheet"> -->
+	<link href="css/shvoip.css" rel="stylesheet">  
+  </head>
+  <body>
+  	<div class="shvoip-top-border"></div>
+  	<div class="container">
+  		<?php require_once 'nav.php';?>  		
+  	</div>
---
+++ inc/nav.php
@@ -0,0 +1,35 @@
+<!-- Static navbar -->
+<div class="navbar navbar-default shvoip-navbar"
+	role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand shvoip-brand" href="/"><img alt="Shvoip voice" src="images/logo.png"></a>
+		</div>
+		<div class="navbar-collapse collapse ">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.html" class="shvoip-blue">Sign In</a></li>				
+				<li><a href="cosa-e.html"  class="shvoip-blue">Sign Up</a></li>				
+			</ul>
+		</div>
+		<div class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right shvoip-border shvoip-nav-align">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="cosa-e.html">Cosa è</a></li>
+				<li><a href="come-funsiona.html">Come Funziona</a></li>
+				<li><a href="prezzi.html">Prezzi</a></li>
+				<li><a href="chi-siamo.html">Chi siamo </a></li>
+				<li><a href="condizoni-duso.html">Condizioni d'uso</a></li>
+				<li><a href="informativa-sulla-privacy.html">Informativa sulla
+						privacy</a></li>
+			</ul>
+		</div>
+		<!--/.nav-collapse -->
+	</div>
+	<!--/.container-fluid -->
+</div>
